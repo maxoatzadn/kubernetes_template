1dcc5a5f6ccd6b826e5115488e9de9a1f7bd5796
maxoatzadn <56143455+maxoatzadn@users.noreply.github.com>
2021-01-26 23:00:19 +0700

    Update apply.sh

--- apply.sh
+++ apply.sh
@@ -2,9 +2,9 @@
 
 sed -i 's/Name/node-01/' template.yaml
 
-# kubectl apply -f template.yaml
+kubectl apply -f template.yaml
 
 
 
 sed -i 's/node-01/node-02/' template.yaml
-# kubectl apply -f template.yaml
+kubectl apply -f template.yaml
