4fbea59aa2b31d7d939becec626f3377efa9ae56
maxoatzadn <56143455+maxoatzadn@users.noreply.github.com>
2021-04-01 22:45:31 +0700

    Add files via upload

---
+++ sertup.sh
@@ -0,0 +1,13 @@
+sudo apt-get update
+sudo apt-get install docker.io -y
+sudo systemctl enable docker
+sudo systemctl start docker
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
+sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+sudo apt-get install kubeadm kubelet kubectl
+sudo apt-mark hold kubeadm kubelet kubectl
+sudo swapoff –a
+#set host name
+sudo hostnamectl set-hostname worker01
+# key change step
+kubeadm join --discovery-token abcdef.1234567890abcdef --discovery-token-ca-cert-hash sha256:1234..cdef 1.2.3.4:6443
